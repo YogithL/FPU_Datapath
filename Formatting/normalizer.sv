a1f8e6bb9f6cc73eb17919eae4945c060dc64fb6
YogithL <lyogith@gmail.com>
2026-06-29 20:01:43 -0500

    Created normalizer and rounder

---
+++ Formatting/normalizer.sv
@@ -0,0 +1,48 @@
+
+module normalizer(
+    input logic[11:0] raw_in,
+    input logic[8:0] exp_in,
+    output logic[7:0] mantissa_out,
+    output logic[8:0] exp_out,
+    output logic G, R, S
+    );
+
+    logic[3:0] shift_amt;
+    logic[11:0] shifted;
+    logic flag_underflow;
+        assign flag_underflow = shift_amt >= exp_in;
+
+    always_comb begin
+        if(raw_in[11]) begin
+            shifted = raw_in >> 1;
+            shifted[0] = raw_in[1] | raw_in[0];
+            exp_out = exp_in + 9'd1;
+        end
+        
+        else begin
+            casez(raw_in[10:0])
+                11'b1??????????: shift_amt = 4'd0;
+                11'b01?????????: shift_amt = 4'd1;
+                11'b001????????: shift_amt = 4'd2;
+                11'b0001???????: shift_amt = 4'd3;
+                11'b00001??????: shift_amt = 4'd4;
+                11'b000001?????: shift_amt = 4'd5;
+                11'b0000001????: shift_amt = 4'd6;
+                11'b00000001???: shift_amt = 4'd7;
+                11'b000000001??: shift_amt = 4'd8;
+                11'b0000000001?: shift_amt = 4'd9;
+                11'b00000000001: shift_amt = 4'd10;
+                default: shift_amt = 4'd0;
+            endcase
+
+            shifted = raw_in << shift_amt;
+            exp_out = exp_in - shift_amt;
+        end
+
+        mantissa_out = shifted[10:3];
+        G = shifted[2];
+        R = shifted[1];
+        S = shifted[0];
+    end
+
+endmodule
---
+++ Formatting/rounder.sv
@@ -0,0 +1,31 @@
+
+module rounder(
+    input logic[7:0] mantissa_in,
+    input logic[8:0] exp_in,
+    input logic G, R, S,
+    output logic[7:0] mantissa_out,
+    output logic[8:0] exp_out
+    );
+
+    logic round_up;
+    logic[8:0] rounded_mantissa;
+    logic flag_overflow;
+        assign flag_overflow = exp_out[8];
+
+    always_comb begin
+        round_up = G & (R | S | mantissa_in[0]);
+
+        rounded_mantissa = {1'b0, mantissa_in} + {8'b0, round_up};
+
+        if(rounded_mantissa[8]) begin
+            mantissa_out = rounded_mantissa[8:1];
+            exp_out = exp_in + 9'd1;
+        end
+
+        else begin
+            mantissa_out = rounded_mantissa[7:0];
+            exp_out = exp_in;
+        end
+    end
+
+endmodule
